05427f804c918ca991f5fdcca125e0a1fe87f249
Alvaro del Castillo <acs@bitergia.com>
2016-06-27 12:16:00 +0200

    [dask_git] Add the complete process: load, lowercase, utc, enrich, write with timings.

--- utils/dask/dask_git.py
+++ utils/dask/dask_git.py
@@ -13,7 +13,6 @@ import sys
 import numpy as np
 import pandas as pd
 
-
 import requests
 
 from copy import deepcopy
@@ -27,6 +26,8 @@ from grimoire.elk.elastic import ElasticSearch, ElasticConnectException
 from grimoire.ocean.elastic import ElasticOcean
 from grimoire.elk.git import GitEnrich
 from grimoire.elk.sortinghat import SortingHat
+
+from perceval.utils import str_to_datetime, datetime_to_utc
 
 
 def get_params():
@@ -138,8 +139,8 @@ def load_data(es, es_index):
 
         for item in ocean:
             items.append(item)
-            if len(items) % 3 == 0: # debug
-                break
+            # if len(items) % 10 == 0: # debug
+            #   break
             if len(items) % 1000 == 0:
                 logging.debug("Items loaded: %i", len(items))
         logging.debug("Items loaded: %i", len(items))
@@ -158,21 +159,22 @@ def lowercase_fields(items):
         new_item = deepcopy(item)
         for key in item.keys():
             new_item[key.lower()] = new_item.pop(key)
-            for dkey in item['data'].keys():
-                if dkey.lower() in ['commit', 'author', 'authordate', 'commitdate']:
-                    # We have Commit and commit in raw index!
-                    # Author and AuthorDate and CommitDate are used in SH
-                    continue
-                new_item['data'][dkey.lower()] = new_item['data'].pop(dkey)
+        for dkey in item['data'].keys():
+            if dkey.lower() in ['commit', 'author', 'authordate', 'commitdate']:
+                # We have Commit and commit in raw index!
+                # Author and AuthorDate and CommitDate are used in SH
+                continue
+            new_item['data'][dkey.lower()] = new_item['data'].pop(dkey)
         new_items.append(new_item)
     return new_items
 
+@timeit
 def utc_dates(items):
     # First step, detect all date fields in data
     dates_fields = []
     for dkey in items[0]['data']:
         try:
-            parser.parse(items[0]['data'][dkey])
+            str_to_datetime(items[0]['data'][dkey])
             dates_fields.append(dkey)
         except Exception:
             pass
@@ -184,20 +186,44 @@ def utc_dates(items):
         for dkey in item['data'].keys():
             if dkey in dates_fields:
                 # date in utc
-                new_item['data'][dkey+"_utc"] = new_item['data'][dkey]
+                date_field = str_to_datetime(new_item['data'][dkey])
+                new_item['data'][dkey+"_utc"] = datetime_to_utc(date_field).isoformat()
                 # timezone in seconds
-                new_item['data'][dkey+"_tz"] = 0
+                new_item['data'][dkey+"_tz"] = date_field.strftime("%z")
         new_items.append(new_item)
 
     return items
 
-def write_data(es_index, eitems):
+@timeit
+def write_data(es, es_index, eitems):
     """ Write the data to ES """
     logging.info("Writing %i items data with demography info to %s", len(eitems), es_index)
 
-    for item in eitems:
-        print (json.dumps(item, indent=4, sort_keys=True))
-        break
+
+    try:
+        print("WRITE: ", es, es_index)
+        elastic = ElasticSearch(es, es_index)
+        url = elastic.url+'/items/_bulk'
+        max_items = elastic.max_items_bulk
+        current = 0
+        bulk_json = ""
+
+
+        for eitem in eitems:
+            if current >= max_items:
+                requests.put(url, data=bulk_json)
+                bulk_json = ""
+                current = 0
+            data_json = json.dumps(eitem)
+            bulk_json += '{"index" : {"_id" : "%s" } }\n' % \
+                (eitem["ocean-unique-id"])
+            bulk_json += data_json +"\n"  # Bulk document
+            current += 1
+        requests.put(url, data = bulk_json)
+    except Exception as ex:
+        logging.error("Can't write data to %s", es)
+        print(ex)
+
 
 
 if __name__ == '__main__':
@@ -206,14 +232,16 @@ if __name__ == '__main__':
 
     ES_IN = args.elastic_url
     ES_IN_INDEX = args.index
+    ES_OUT = ES_IN
+    ES_OUT_INDEX = ES_IN_INDEX
     SORTINGHAT_DB = args.db_sortinghat
     PROJECTS_DB = args.db_projects_map
-    ES_OUT = ''
 
     DASK_GRAPH = {
         'es_read': ES_IN,
+        'es_write': ES_OUT,
         'es_index_read': ES_IN_INDEX,
-        'es_es_index_write': ES_OUT,
+        'es_es_index_write': ES_OUT_INDEX,
         'es_pandas_index_write': ES_OUT+"_pandas",
         'load': (load_data, 'es_read', 'es_index_read'),
         'lowercase_fields' : (lowercase_fields, 'load'),
@@ -221,8 +249,9 @@ if __name__ == '__main__':
         'sortinghat': (identities2sh, 'utc_dates', SORTINGHAT_DB),
         'enrich_es': (enrich_git, 'sortinghat', PROJECTS_DB, SORTINGHAT_DB),
         'enrich_pandas': (enrich_git_pandas, 'sortinghat'),
-        'write_es_es': (write_data, 'es_es_index_write', 'enrich_es'),
+        'write_es_es': (write_data, 'es_write','es_es_index_write', 'enrich_es'),
         'write_es_pandas': (write_data, 'es_pandas_index_write', 'enrich_pandas')
     }
 
-    get(DASK_GRAPH, ['write_es_es','write_es_pandas'])
+    # get(DASK_GRAPH, ['write_es_es','write_es_pandas'])
+    get(DASK_GRAPH, ['write_es_es'])
