73e5bd5f56acb49b148ca558e52ccb0a3fb11a50
Alvaro del Castillo <acs@bitergia.com>
2016-10-30 23:01:16 +0100

    [enrich][sortinghat_ds] Add source and profile information to identities

--- grimoire/elk/sortinghat_ds.py
+++ grimoire/elk/sortinghat_ds.py
@@ -34,19 +34,38 @@ class SortingHatDSEnrich(Enrich):
     def get_field_unique_id(self):
         return "uuid"
 
-    def get_elastic_mappings(self):
+    def get_field_event_unique_id(self):
+        return 'id'
 
-        mapping = """
-        {
-            "properties": {
-                "name_analyzed": {
-                  "type": "string",
-                  "index":"analyzed"
-                  }
-           }
-        } """
+    def get_rich_events(self, item):
+        """ Create a new eventry (event) per identity in the unique identity """
 
-        return {"items":mapping}
+        identities = []
+
+        # To get values from the task
+        uid = self.get_rich_item(item)
+
+        for i in item['data']["identities"]:
+            identity = {}
+            # Common fields with the item
+            # Needed for incremental updates from the item
+            identity['metadata__updated_on'] = item['metadata__updated_on']
+            identity['origin'] = item['origin']
+            # Common fields with the uid
+            cfields = ['profile_name', 'profile_email']
+            for f in cfields:
+                identity[f] = uid[f]
+
+            # enrollments
+            identity['enrollments'] = uid['enrollments']
+            # Real event data
+            copy_fields = ['id', 'uuid', 'email', 'username', 'name', 'source']
+            for f in copy_fields:
+                identity[f] = i[f]
+
+            identities.append(identity)
+
+        return identities
 
     def get_rich_item(self, item):
         eitem = {}
@@ -75,10 +94,20 @@ class SortingHatDSEnrich(Enrich):
 
         # Enrich dates
         eitem["update_date"] = parser.parse(item["metadata__updated_on"]).isoformat()
-        # Identities
+        # Identities: will break down in events analysis
         eitem["nidentities"] = len(uidentity["identities"])
         # Enrollments
+        eitem["enrollments"] = None
         if len(uidentity["enrollments"])>0:
-            pass
-            
+            eitem["enrollments"] = ''
+            for e in uidentity["enrollments"]:
+                eitem["enrollments"] += ","+e['organization']
+            eitem["enrollments"] = eitem["enrollments"][1:]
+        # Profile
+        eitem['profile_name'] = None
+        eitem['profile_email'] = None
+        if uidentity['profile']:
+            eitem['profile_name'] = uidentity['profile']['name']
+            eitem['profile_email'] = uidentity['profile']['email']
+
         return eitem
